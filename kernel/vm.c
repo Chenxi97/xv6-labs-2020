782dbcb4d5d34f641d31a6d85a82db048ca50b74
Chenxi97 <chenxswjtu@gmail.com>
2020-11-05 17:06:30 +0800

    Finish Print a page table (easy)

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,25 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void printwalk(pagetable_t pagetable, uint16 level){
+  for(int i=0;i<512;i++){
+    pte_t pte=pagetable[i];
+    if(pte&PTE_V){
+      for(int l=1;l<level;l++){
+        printf(".. ");
+      }
+      uint64 child=PTE2PA(pte);
+      printf("..%d: pte %p pa %p\n",i,pte,child);
+      if((pte&(PTE_R|PTE_W|PTE_X))==0){
+        // this PTE points to a lower-level page table
+        printwalk((pagetable_t)child,level+1);
+      }
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable){
+  printf("page table %p\n",pagetable);
+  printwalk(pagetable,1);
+}
