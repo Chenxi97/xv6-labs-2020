7bccc983aa9e15d293b272bef552a5f9c6decbac
Chenxi97 <chenxswjtu@gmail.com>
2020-11-10 16:02:24 +0800

    Finish 2.Using threads (moderate)

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,8 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+// declare locks for table bucket
+pthread_mutex_t lock[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
@@ -40,6 +42,7 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&lock[i]);       // acquire lock
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -53,6 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&lock[i]);     // release lock
 }
 
 static struct entry*
@@ -113,6 +117,11 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  // initialize the bucket locks
+  for(int i=0;i<NBUCKET;i++){
+    pthread_mutex_init(&lock[i], NULL);
   }
 
   //
